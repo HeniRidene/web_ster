af79ae0f6690ac2c869edee46816b544cf9363a1
HeniRidene <116555865+HeniRidene@users.noreply.github.com>
2024-02-19 21:50:16 +0100

    php files

--- atelier HTML/inscription.js
+++ atelier HTML/inscription.js
@@ -1,20 +1,32 @@
-
 const form = document.getElementById("form");
 const nomInput = document.getElementById("nom");
+const prenomInput = document.getElementById("prenom");
 const emailInput = document.getElementById("email");
-const motDePasseInput = document.getElementById("mot_de_passe");
+const telephoneInput = document.getElementById("telephone");
+const datenaissanceInput = document.getElementById("date_naissance");
+const motDePasseInput = document.getElementById("mot_de_passe"); 
 
 
 form.addEventListener("submit", function(event) {
     event.preventDefault(); 
 
     const nom = nomInput.value.trim();
-    if (nom === "" || nom.length < 3) {
-        document.getElementById("nameError").textContent = "Le nom doit contenir au moins 3 caractères.";
+    if (nom === "" || nom.length < 0) {
+        document.getElementById("nameError").textContent = "vous devez ecrire votre nom.";
         return;
     } else {
         document.getElementById("nameError").textContent = ""; 
     }
+
+
+    const prenom = prenomInput.value.trim();
+    if (prenom === "" || prenom.length < 0) {
+        document.getElementById("prenomError").textContent = "vous devez ecrire votre prenom.";
+        return;
+    } else {
+        document.getElementById("prenomError").textContent = ""; 
+    }
+
 
     const email = emailInput.value.trim();
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -22,12 +34,43 @@ form.addEventListener("submit", function(event) {
         alert("Veuillez entrer une adresse e-mail valide.");
         return;
     }
+    else {
+        document.getElementById("date_naissanceError").textContent = ""; 
+    }
+    
 
-    const motDePasse = motDePasseInput.value;
-    const motDePasseRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    if (!motDePasseRegex.test(motDePasse)) {
-        alert("Le mot de passe doit contenir au moins 8 caractères, dont une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial.");
+    const telephone = telephoneInput.value.trim();
+    const telephoneRegex = /^[0-9]{10}$/;
+    if (!telephoneRegex.test(telephone)) {
+        alert("Veuillez entrer un numero de telephone valide.");
         return;
     }
-    alert("Formulaire soumis avec succès !");
+    else {
+        document.getElementById("telephoneError").textContent = ""; 
+    }
+
+
+    const date_naissance = datenaissanceInput.value;
+    const dateNaissanceRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!dateNaissanceRegex.test(date_naissance)) {
+        alert("Veuillez entrer une date de naissance valide.");
+        return; 
+    }
+    else {
+        document.getElementById("date_naissanceError").textContent = ""; 
+    }
+    
+
+    const mot_de_passe = motDePasseInput.value;
+    const motDePasseRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!motDePasseRegex.test(mot_de_passe)) {
+        alert("Le mot de passe doit contenir au moins 8 caracteres, dont une lettre minuscule, une lettre majuscule, un chiffre et un caractere special.");
+        return;    
+    }
+    else {
+        document.getElementById("mot_de_passeError").textContent = ""; 
+    }
+
+    alert("Formulaire soumis avec succes !");
+    
 });
